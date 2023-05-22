91eb91f52f404ec560cde64a43a60410f15ea406
Jack Windham <jckwind11@gmail.com>
2023-05-21 22:28:16 -0400

    playing around

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/app/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'blue': '#000080', // Royal Blue
+                'gold': '#FFD700', // Gold
+                'pale-gold': '#FFEBCD', // Pale gold
+                'blackish': '#212121', // Blackish
+                'white': '#FFFFFF', // White
+            },
+            fontFamily: {
+                // Add your custom fonts here
+                // 'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    variants: {},
+    plugins: [],
 }
